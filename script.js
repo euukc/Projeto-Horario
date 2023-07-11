ba25de7176ea1913527a3edafb68b5e00adddb2a
euukc <56362484+euukc@users.noreply.github.com>
2023-07-11 19:17:59 -0300

    imagens

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = '22'
+    var hora = '23'
         msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12){
         img.src = 'fotomanha.png'
-        
-    } else if (hora >= 12 && hora <= 18){
+
+    } else if (hora >= 12 && hora <18 ){
         img.src = 'fototarde.png'
+
     } else {
         img.src = 'fotonoite.png'
     }
