63241690cbe843cb62127423b22a741eb38dcea8
euukc <56362484+euukc@users.noreply.github.com>
2023-07-15 12:32:00 -0300

    data.getHours

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = '22'
+    var hora = data.getHours()
+    //var hora = '22'
         msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora <12) {
